48d0e66c970672e823f67a267ed73a06e6279cd9
Erik Hanchett <icystorm@gmail.com>
2015-11-27 01:48:28 -0500

    updated to 2.2

--- app/routes/r.js
+++ app/routes/r.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function(params){
-		return $.getJSON(`https://www.reddit.com/r/${params.subreddit}.json`);
+		return Ember.$.getJSON(`https://www.reddit.com/r/${params.subreddit}.json`);
 	},
 	afterModel: function(r){
 		r.subreddit = r.data.children[0].data.subreddit || '';
